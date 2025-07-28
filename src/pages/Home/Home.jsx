0f251ff8029d2a522b6e2b2c7ad1ddf237b8b652
np-naveen <102174275+np-naveen@users.noreply.github.com>
2025-07-28 11:32:38 +0530

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,15 @@
 import React, { useEffect } from "react";
 
-import { Card, Flex, Input, Button, Radio, Row, Col, Modal } from "antd";
+import { Card, Flex, Input, Button, Radio, Spin } from "antd";
 import { IdcardOutlined, TableOutlined } from "@ant-design/icons";
 import { useSelector, useDispatch } from "react-redux";
 
-import { ListCard, UserCardContainer } from "../../Styles";
+import { ListCard, SpinContainer, UserCardContainer } from "../../Styles";
 import constant from "../../Utils/constant";
 import { fetchUserData } from "../../ApiUtils/service";
 import UserCard from "../../components/UserCard";
 import UserForm from "../../components/UserForm";
+import Table from "../../components/AntdTable";
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -21,7 +22,7 @@ const Home = () => {
     });
   };
 
-  const { mode, pageNo, userData, modalAction } = useSelector(
+  const { mode, pageNo, userData, modalAction, loading } = useSelector(
     (state) => state.home
   );
 
@@ -30,11 +31,24 @@ const Home = () => {
   }, []);
 
   const init = async () => {
-    const userData = await fetchUserData(pageNo);
-    dispatch({
-      type: constant.USER_DATA,
-      payload: userData,
-    });
+    try {
+      dispatch({
+        type: constant.LOADING,
+        payload: true,
+      });
+      const userData = await fetchUserData(pageNo);
+      dispatch({
+        type: constant.USER_DATA,
+        payload: userData,
+      });
+    } catch (e) {
+      console.log(e);
+    } finally {
+      dispatch({
+        type: constant.LOADING,
+        payload: false,
+      });
+    }
   };
 
   const handleCreateUser = () => {
@@ -78,18 +92,28 @@ const Home = () => {
             </Radio.Button>
           </Radio.Group>
         </div>
-        {userData && (
+        {loading ? (
           <>
-            {mode === "table" ? (
-              <Card>{/* Render table view of userData */}</Card>
-            ) : (
-              <UserCardContainer>
-                <Flex wrap="wrap" gap="16px">
-                  {userData.map((user) => (
-                    <UserCard key={user.id} user={user} />
-                  ))}
-                </Flex>
-              </UserCardContainer>
+            <SpinContainer>
+              <Spin tip="Loading" size="large" />
+            </SpinContainer>
+          </>
+        ) : (
+          <>
+            {userData && (
+              <>
+                {mode === "table" ? (
+                  <Card><Table data={userData}/></Card>
+                ) : (
+                  <UserCardContainer>
+                    <Flex wrap="wrap" gap="16px">
+                      {userData.map((user) => (
+                        <UserCard key={user.id} user={user} />
+                      ))}
+                    </Flex>
+                  </UserCardContainer>
+                )}
+              </>
             )}
           </>
         )}
