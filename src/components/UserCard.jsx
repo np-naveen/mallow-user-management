54a30f8cbc63cb497aa6ec1a3ec5a64df263f945
np-naveen <102174275+np-naveen@users.noreply.github.com>
2025-07-28 15:33:33 +0530

    Update UserCard.jsx

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,37 +1,30 @@
 import React, { useEffect } from "react";
 import { Card, Button } from "antd";
 import { useSelector, useDispatch } from "react-redux";
+import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
 
 import { Overlay, ProfileCard, ProfileImage } from "../Styles";
 import UserForm from "./UserForm";
 import constant from "../Utils/constant";
 
-const UserCard = ({ user }) => {
-  const [isEdit, setIsEdit] = React.useState(false);
+const UserCard = ({ user,handleDelete }) => {
   const dispatch = useDispatch();
   const { modalAction } = useSelector((state) => state.home);
 
   const handleEdit = () => {
-    console.log("handleEdit");
-    setIsEdit(true);
     dispatch({
       type: constant.MODAL_ACTION,
       payload: { visible: true, action: "edit", data: user },
     });
   };
 
-  useEffect(() => {
-    console.log(isEdit, modalAction);
-  }, [isEdit, modalAction]);
   return (
     <>
       <Card key={user.id} style={{ width: 300 }}>
         <ProfileCard>
           <Overlay>
-            <Button type="primary" onClick={handleEdit}>
-              Edit
-            </Button>
-            <Button danger>Delete</Button>
+            <Button shape="circle" type="primary" onClick={handleEdit} icon={<EditOutlined />} />
+            <Button shape="circle" type="primary" danger onClick={()=>{handleDelete(user)}} icon={<DeleteOutlined />} />
           </Overlay>
           <ProfileImage src={user.avatar} alt={user.name} preview={false} />
           <h3>{`${user.first_name} ${user.last_name}`}</h3>
